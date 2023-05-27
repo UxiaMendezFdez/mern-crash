0de6b32b90090fdf484c42a7008cb9b323cb3525
UxiaMendezFdez <uxia.men.fdez@gmail.com>
2023-05-27 16:57:45 +0200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => res.send("server is ready"));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
